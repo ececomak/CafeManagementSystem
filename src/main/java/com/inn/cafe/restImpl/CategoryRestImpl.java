bf50595166223992545fa0bdf7770e8bc3a8f2f5
Gizem Ece Comak <147514476+ececomak@users.noreply.github.com>
2025-04-21 23:46:06 +0300

    a bug just fixed heheyyy

--- src/main/java/com/inn/cafe/restImpl/CategoryRestImpl.java
+++ src/main/java/com/inn/cafe/restImpl/CategoryRestImpl.java
@@ -7,16 +7,19 @@ import com.inn.cafe.utils.CafeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping("/category")
 public class CategoryRestImpl implements CategoryRest {
 
     @Autowired
     CategoryService categoryService;
 
-    @Override
-    public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
+    @PostMapping("/add")
+    public ResponseEntity<String> addNewCategory(@RequestBody Map<String, String> requestMap) {
         try {
             return categoryService.addNewCategory(requestMap);
         } catch (Exception e) {
